ff7b12f66e706c65651a1c74de1e88405e183fd0
Grégoire <gregoire.david@ymail.com>
2020-06-11 16:43:30 +0200

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,13 @@ git checkout master
 git pull
 
 # installation des packages
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirement.txt
 
 # Créer les tables et le repo des migrations (sqlite3 par défaut si la variable env DATABASE_URL n'est pas fixée)
 flask db init
-flask db flask db migrate -m "Initialisation de la db"
+flask db migrate -m "Initialisation de la db"
 flask db upgrade
 
 # Créer les dossiers data et télécharger les données brutes
